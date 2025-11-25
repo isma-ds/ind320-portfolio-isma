a1b96e6c5cd5da3ae337c8057172c76040024629
Isma Sohail <IsmaSohail@gmail.com>
2025-11-25 11:49:15 +0500

    Repo Cleaned

--- Desktop/ind320_part2_starter/streamlit_app/pages/02_Page2.py
+++
@@ -1,4 +0,0 @@
-
-import streamlit as st
-st.title("Page 2 — (Placeholder)")
-st.write("Keep your Part 1 content here. You can move extra content to Page 5.")
--- Desktop/ind320_part2_starter/streamlit_app/pages/03_Page3.py
+++
@@ -1,4 +0,0 @@
-
-import streamlit as st
-st.title("Page 3 — (Placeholder)")
-st.write("Keep your Part 1 content here. You can move extra content to Page 5.")
